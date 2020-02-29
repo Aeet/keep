20524136965ed81f82d3d5d2f5cf557d04232eea
Aeet <abdelghani.ettalhaoui@gmail.com>
2020-02-29 19:55:12 +0100

    install react-navigation and other dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 // import Notes from './components/notes';
 import NoteBuilder from './components/notes/builder';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{ flex: 1 }}>
         {/* <Notes /> */}
         <NoteBuilder />
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
 
